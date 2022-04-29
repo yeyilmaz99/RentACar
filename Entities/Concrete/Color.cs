2608b41b551dc6e008faff3c58470c8d9bbab5f3
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-04-29 04:49:29 +0300

    ReCapProject

--- Entities/Concrete/Color.cs
+++ Entities/Concrete/Color.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Entities.Concrete
 {
-    public class Color
+    public class Color: IEntity
     {
         public int ColorId { get; set; }
         public string ColorName { get; set; }
