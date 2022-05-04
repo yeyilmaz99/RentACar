727b8deb009d3af59cf7f9400faac366ef210c9e
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-04 18:26:20 +0300

    MyDatabaseContext added

---
+++ DataAccess/Concrete/EntityFramework/MyDatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class MyDatabaseContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=MyDatabase;Trusted_Connection=true");
+        }
+    }
+}
