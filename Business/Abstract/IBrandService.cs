cae3a7473e626ecd9b0bb7664df82d33163ddb41
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-08 19:25:41 +0300

    9.gün ödev

--- Business/Abstract/IBrandService.cs
+++ Business/Abstract/IBrandService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace Business.Abstract
 {
     public interface IBrandService
     {
+        List<Brand> GetAll();
+        Brand GetById(int id);
+        void Add(Brand car);
+        void Update(Brand car);
+        void Delete(Brand car);
+
     }
 }
