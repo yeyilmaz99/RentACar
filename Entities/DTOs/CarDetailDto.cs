fd115d2f9bbbdf0af1aa5b61ada44c4a5c032c71
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-08 19:19:03 +0300

    9.gün ödev

---
+++ Entities/DTOs/CarDetailDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs
+{
+    public class CarDetailDto
+    {
+        public string CarName { get; set; }
+        public string BrandName { get; set; }
+        public string ColorName { get; set; }
+        public double DailyPrice { get; set; }
+    }
+}
