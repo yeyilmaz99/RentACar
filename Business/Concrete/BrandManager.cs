397957372ac2988e675dda1d9d8be0b7a003acc7
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-08 19:31:40 +0300

    9.gün ödev

---
+++ Business/Concrete/BrandManager.cs
@@ -0,0 +1,38 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class BrandManager : IBrandService
+    {
+        public void Add(Brand car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Brand car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Brand> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Brand GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Brand car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
