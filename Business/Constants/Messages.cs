f5ba2b9fe347e8b33cc3ae841f67e7bfab9de7b4
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-14 17:37:38 +0300

    10. gün ödev

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInvalid = "Ürün Eklendi";
+        public static string MaintenanceTime = "Ürün Eklendi";
+        public static string ProductsListed = "Ürün Eklendi";
+    }
+}
