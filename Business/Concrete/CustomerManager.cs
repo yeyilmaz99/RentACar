6c7c21e8b9a39d80ec82b0a6b88341bf2e809476
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-15 17:22:04 +0300

    10.gün ödev

---
+++ Business/Concrete/CustomerManager.cs
@@ -0,0 +1,65 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CustomerManager : ICustomerService
+    {
+        ICustomerDal _customerDal;
+
+        public CustomerManager(ICustomerDal customerDal)
+        {
+            _customerDal = customerDal;
+        }
+
+        public IResult Add(Customer customer)
+        {
+            if (customer.CompanyName.Length < 2)
+            {
+                return new ErrorResult();
+            }
+            _customerDal.Add(customer);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(Customer customer)
+        {
+            if(DateTime.Now.Hour == 22)
+            {
+                return new ErrorResult();
+            }
+            _customerDal.Delete(customer);
+            return new SuccessResult();
+
+        }
+
+        public IDataResult<List<Customer>> GetAll()
+        {
+            return new SuccessDataResult<List<Customer>>(_customerDal.GetAll());
+
+        }
+
+        public IDataResult<Customer> GetById(int id)
+        {
+            return new SuccessDataResult<Customer>(_customerDal.Get(c => c.UserId == id));
+        }
+
+        public IResult Update(Customer customer)
+        {
+            if(DateTime.Now.Hour == 22)
+            {
+                return new ErrorResult();
+            }
+            _customerDal.Update(customer);
+            return new SuccessResult();
+        }
+    }
+}
