6b209743f0b5787db24e2c90542263cc39963903
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-15 16:44:13 +0300

    10.gün ödev

---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Utilities.Results.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        IDataResult<List<User>> GetAll();
+        IDataResult<User> GetById(int id);
+        IResult Add(User brand);
+        IResult Update(User brand);
+        IResult Delete(User brand);
+    }
+}
