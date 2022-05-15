eacb81d5deca9221498ff73f4b3161d00a182b36
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-15 17:21:37 +0300

    10.gün ödev

--- Business/Abstract/ICustomerService.cs
+++ Business/Abstract/ICustomerService.cs
@@ -12,8 +12,8 @@ namespace Business.Abstract
     {
         IDataResult<List<Customer>> GetAll();
         IDataResult<Customer> GetById(int id);
-        IResult Add(Customer brand);
-        IResult Update(Customer brand);
-        IResult Delete(Customer brand);
+        IResult Add(Customer customer);
+        IResult Update(Customer customer);
+        IResult Delete(Customer customer);
     }
 }
