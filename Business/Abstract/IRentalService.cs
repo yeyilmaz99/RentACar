d239240ee48c80a22cf786e1d264a7424685e7c5
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-15 16:46:22 +0300

    10.gün ödev

---
+++ Business/Abstract/IRentalService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Utilities.Results.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IRentalService
+    {
+        IDataResult<List<Rental>> GetAll();
+        IDataResult<Rental> GetById(int id);
+        IResult Add(Rental brand);
+        IResult Update(Rental brand);
+        IResult Delete(Rental brand);
+    }
+}
