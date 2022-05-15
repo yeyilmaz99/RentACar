0c18e097e3b3ffd559bdba1cfffc86903166b88c
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-15 17:22:13 +0300

    10.gün ödev

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,59 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        IRentalDal _rentalDal;
+
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+
+        public IResult Add(Rental rental)
+        {
+            _rentalDal.Add(rental);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            if(DateTime.Now.Hour == 22)
+            {
+                return new ErrorResult();
+            }
+            _rentalDal.Delete(rental);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            return new SuccessDataResult<List<Rental>>(_rentalDal.GetAll());
+        }
+
+        public IDataResult<Rental> GetById(int id)
+        {
+            return new SuccessDataResult<Rental>(_rentalDal.Get(r => r.Id == id));
+        }
+
+        public IResult Update(Rental rental)
+        {
+            if(DateTime.Now.Hour == 22)
+            {
+                return new ErrorResult();
+            }
+            _rentalDal.Update(rental);
+            return new SuccessResult();
+        }
+    }
+}
