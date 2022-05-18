58ead8d9c4e91ffe111bce6f826d608d841e87ee
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-18 19:59:24 +0300

    11.gün ödev

--- WebAPI/Controllers/RentalsController.cs
+++ WebAPI/Controllers/RentalsController.cs
@@ -25,5 +25,16 @@ namespace WebAPI.Controllers
             }
             return BadRequest(result);  
         }
+
+        [HttpGet("getbyid")]
+        public IActionResult Get(int id)
+        {
+            var result = _rentalService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
     }
 }
