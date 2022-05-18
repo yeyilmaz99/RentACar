1d8e7bfd4c1415e2c8c235f4c6cd71722bfb81ab
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-18 19:13:35 +0300

    11.gün ödev

---
+++ WebAPI/Controllers/BrandsController.cs
@@ -0,0 +1,36 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        IBrandService _brandService;
+
+        public BrandsController(IBrandService brandService)
+        {
+            _brandService = brandService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult Get()
+        {
+            var result = _brandService.GetAll();
+            if(result.Success == true)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult Get()
+        {
+            var result
+        }
+
+    }
+}
