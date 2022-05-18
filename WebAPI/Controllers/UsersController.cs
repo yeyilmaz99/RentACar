f5eca8340786d23c5d2e4ebe2e044a4479f819e5
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-18 19:44:34 +0300

    11.gün ödev

--- WebAPI/Controllers/UsersController.cs
+++ WebAPI/Controllers/UsersController.cs
@@ -26,5 +26,17 @@ namespace WebAPI.Controllers
             return BadRequest(result);
         }
 
+        [HttpGet("getbyid")]
+        public IActionResult Get(int id)
+        {
+            var result = _userService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+        }
+
     }
 }
