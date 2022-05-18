744a5d1f585d86f9a973009e59f69d20463b3aae
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-18 19:02:55 +0300

    11.gün ödev

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        ICarService _carservice;
+
+        public CarsController(ICarService carservice)
+        {
+            _carservice = carservice;
+        }
+
+
+        [HttpGet("getall")]
+        public IActionResult Get()
+        {
+            var result = _carservice.GetAll();
+            if(result.Success == true)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+
+
+
+    }
+}
