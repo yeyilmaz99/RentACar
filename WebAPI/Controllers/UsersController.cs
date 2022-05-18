b318fe0673fb317b4f1aea9eb96154781842a9ae
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-18 19:42:23 +0300

    11.gün ödev

---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult Get()
+        {
+            var result = _userService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+    }
+}
