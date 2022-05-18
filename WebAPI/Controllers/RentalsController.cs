ed1de5ca06257a057eea8d2bbef9977538e58fa3
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-18 19:58:13 +0300

    11.gün ödev

---
+++ WebAPI/Controllers/RentalsController.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentalsController : ControllerBase
+    {
+        IRentalService _rentalService;
+
+        public RentalsController(IRentalService rentalService)
+        {
+            _rentalService = rentalService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult Get()
+        {
+            var result = _rentalService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);  
+        }
+    }
+}
