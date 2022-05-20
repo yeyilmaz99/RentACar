bc450148198f7afb196e9ac8d58e06db944b3b7c
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-21 01:14:13 +0300

    12.gün ödev FluentValidation CustomerValidator

--- Business/Concrete/CustomerManager.cs
+++ Business/Concrete/CustomerManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns;
 using Core.Utilities.Results.Abstract;
 using Core.Utilities.Results.Concrete;
 using DataAccess.Abstract;
@@ -22,10 +24,7 @@ namespace Business.Concrete
 
         public IResult Add(Customer customer)
         {
-            if (customer.CompanyName.Length < 2)
-            {
-                return new ErrorResult();
-            }
+            ValidationTool.Validate(new CustomerValidator(), customer);
             _customerDal.Add(customer);
             return new SuccessResult();
         }
