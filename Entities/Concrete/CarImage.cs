99480cf2860ac32a5b38b383261bb8331d652c70
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-26 04:11:54 +0300

    13.gün ödev

---
+++ Entities/Concrete/CarImage.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class CarImage : IEntity
+    {
+        public int Id { get; set; }
+        public string ImageName { get; set; }
+        public int CarId { get; set; }
+        [NotMapped]
+        public IFormFile Image { get; set; }
+        public string ImagePath { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
