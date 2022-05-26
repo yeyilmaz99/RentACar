c603648dac9dcac6c5ce4bbd0d4203413749342c
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-26 04:11:56 +0300

    13.gün ödev

---
+++ Entities/DTOs/ImageAddDto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs
+{
+    public class ImageAddDto
+    {
+        public int CarId { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
