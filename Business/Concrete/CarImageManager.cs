de14d585f19cd34eb958d685c8449c232b238954
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-26 04:11:28 +0300

    13.gün ödev

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,70 @@
+﻿using Business.Abstract;
+using Core.Utilities.Business;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+
+        ICarImageDal _carImageDal;
+
+        public CarImageManager(ICarImageDal carImageDal)
+        {
+            _carImageDal = carImageDal;
+        }
+
+        public IResult Add(CarImage carImage)
+        {
+            IResult result = BusinessRules.Run(CheckIfCarImagesFull(carImage.CarId));
+            if (result != null)
+            {
+                return result;
+            }
+
+            _carImageDal.Add(carImage);
+            return new SuccessResult();
+
+        }
+
+        public IResult Delete(CarImage carImage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<CarImage>> GetAll()
+        {
+            return new SuccessDataResult<List<CarImage>>(_carImageDal.GetAll());
+        }
+
+        public IDataResult<List<CarImage>> GetAllImagesByCarId(int carId)
+        {
+            return new SuccessDataResult<List<CarImage>>(_carImageDal.GetAll(c => c.CarId == carId));
+        }
+
+        public IResult Update(CarImage carImage)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IResult CheckIfCarImagesFull(int carId)
+        {
+            var result = _carImageDal.GetAll(c => c.CarId == carId);
+            if (result.Count >= 5)
+            {
+                return new ErrorResult();
+            }
+            return new SuccessResult();
+        }
+    }
+}
