d6136e4a6682d6d7bc08834c1ef78393311bb260
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-26 04:11:26 +0300

    13.gün ödev

---
+++ Business/Abstract/ICarImageService.cs
@@ -0,0 +1,20 @@
+﻿using Core.Utilities.Results.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface ICarImageService
+    {
+        IDataResult<List<CarImage>> GetAllImagesByCarId(int carId);
+        IDataResult<List<CarImage>> GetAll();
+        IResult Add(CarImage carImage);
+        IResult Update(CarImage carImage);
+        IResult Delete(CarImage carImage);
+    }
+}
