9661eec493afd853f5240528d59bf6108c795b87
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-27 23:53:33 +0300

    14.gün ödev

--- Core/Entities/Concrete/User.cs
+++ Core/Entities/Concrete/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Core.Entities.Concrete
 {
-    public class User
+    public class User : IEntity
     {
         public int  Id { get; set; }
         public string FirstName { get; set; }
