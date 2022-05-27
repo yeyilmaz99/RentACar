0c4d529d29233d62bb6f15c74226e70fbb4ebc97
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-27 23:52:58 +0300

    14.gün ödev

---
+++ Core/Entities/Concrete/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Concrete
+{
+    public class User
+    {
+        public int  Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public bool Status { get; set; }
+    }
+
+    public class OperationClaim 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    public class UserOperationClaim
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
+    }
+
+
+
+}
