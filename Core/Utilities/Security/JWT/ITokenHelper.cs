f14950ccf451d162d2c4cc76e85d32c3f36c71aa
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-28 00:42:32 +0300

    14.gün ödev Jwt

---
+++ Core/Utilities/Security/JWT/ITokenHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities.Concrete;
+
+namespace Core.Utilities.Security.JWT
+{
+    public interface ITokenHelper
+    {
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaims);
+    }
+}
