abc81acff78bb91b387fe61a360c2a6d6f8b1f58
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-28 18:51:47 +0300

    14.gün ödev BusinessAspects

--- DataAccess/Abstract/IUserDal.cs
+++ DataAccess/Abstract/IUserDal.cs
@@ -1,15 +1,15 @@
-﻿using Core.DataAccess;
-using Entities.Concrete;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.DataAccess;
+using Core.Entities.Concrete;
 
 namespace DataAccess.Abstract
 {
-    public interface IUserDal: IEntityRepository<User>
+    public interface IUserDal : IEntityRepository<User>
     {
-
+        List<OperationClaim> GetClaims(User user);
     }
 }
