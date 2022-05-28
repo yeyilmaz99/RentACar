9a4a4449edfa995aa7a593826a1e2498040274fc
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-28 18:51:53 +0300

    14.gün ödev BusinessAspects

--- DataAccess/Concrete/EntityFramework/MyDatabaseContext.cs
+++ DataAccess/Concrete/EntityFramework/MyDatabaseContext.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Entities.Concrete;
 
 namespace DataAccess.Concrete.EntityFramework
 {
@@ -20,9 +21,12 @@ namespace DataAccess.Concrete.EntityFramework
         public DbSet<Color> Colors { get; set; }
         public DbSet<Brand> Brands { get; set; }
         public DbSet<Customer> Customers { get; set; }
-        public DbSet<User> Users { get; set; }
         public DbSet<Rental> Rentals { get; set; }
         public DbSet<CarImage> CarImages { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+
 
     }
 }
