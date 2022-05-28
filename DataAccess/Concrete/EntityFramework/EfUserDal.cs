c1c467cd06070221260f7b79c092e1bbddd5ff88
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-28 18:51:51 +0300

    14.gün ödev BusinessAspects

--- DataAccess/Concrete/EntityFramework/EfUserDal.cs
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -7,10 +7,24 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Entities.Concrete;
 
 namespace DataAccess.Concrete.EntityFramework
 {
     public class EfUserDal : EfEntityRepositoryBase<User, MyDatabaseContext>, IUserDal
     {
+        public List<OperationClaim> GetClaims(User user)
+        {
+            using (var context = new MyDatabaseContext())
+            {
+                var result = from operationClaim in context.OperationClaims
+                    join userOperationClaim in context.UserOperationClaims
+                        on operationClaim.Id equals userOperationClaim.OperationClaimId
+                    where userOperationClaim.UserId == user.Id
+                    select new OperationClaim { Id = operationClaim.Id, Name = operationClaim.Name };
+                return result.ToList();
+
+            }
+        }
     }
 }
