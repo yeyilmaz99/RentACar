63c564267ef007e922a7ef2b2efd7ec8c4b356e2
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-28 18:51:22 +0300

    14.gün ödev BusinessAspects

--- Business/Abstract/IUserService.cs
+++ Business/Abstract/IUserService.cs
@@ -1,19 +1,17 @@
-﻿using Core.Utilities.Results.Abstract;
-using Entities.Concrete;
+﻿using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Entities.Concrete;
 
 namespace Business.Abstract
 {
     public interface IUserService
     {
-        IDataResult<List<User>> GetAll();
-        IDataResult<User> GetById(int id);
-        IResult Add(User user);
-        IResult Update(User user);
-        IResult Delete(User user);
+        List<OperationClaim> GetClaims(User user);
+        void Add(User user);
+        User GetByMail(string email);
     }
 }
