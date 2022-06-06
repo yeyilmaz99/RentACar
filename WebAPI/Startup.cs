3a20ecd9bfd655619b528b2e78ab6eb4eb7cbfe2
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-06-06 22:06:50 +0300

    17.gün ödev DTO's

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -41,7 +41,8 @@ namespace WebAPI
 
             services.AddControllers();
 
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddCors();
+
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -82,6 +83,8 @@ namespace WebAPI
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
+
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
