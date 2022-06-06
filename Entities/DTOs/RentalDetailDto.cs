95c2cedf5f1041d61de9375af10daae3294c2de6
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-06-06 22:06:40 +0300

    17.gün ödev DTO's

---
+++ Entities/DTOs/RentalDetailDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs
+{
+    public class RentalDetailDto
+    {
+        public int Id { get; set; }
+        public string BrandName  { get; set; }
+        public string FirstName { get; set; }
+        public string CompanyName { get; set; } 
+        public string LastName { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public double DailyPrice { get; set; }
+
+    }
+}
