25dacb402f9202a6111677266015a0be143c86cb
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-06-06 22:06:26 +0300

    17.gün ödev DTO's

--- Business/Abstract/IRentalService.cs
+++ Business/Abstract/IRentalService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities.DTOs;
 
 namespace Business.Abstract
 {
@@ -12,8 +13,10 @@ namespace Business.Abstract
     {
         IDataResult<List<Rental>> GetAll();
         IDataResult<Rental> GetById(int id);
+        IDataResult<List<RentalDetailDto>> GetRentalDetails();
         IResult Add(Rental rental);
         IResult Update(Rental rental);
         IResult Delete(Rental rental);
+        
     }
 }
