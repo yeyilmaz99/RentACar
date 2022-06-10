24fc5e7ca6ba2b37b062b2a45e904eed9af0b2d4
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-06-10 21:08:25 +0300

    Default Image Logic

--- Business/Concrete/BrandManager.cs
+++ Business/Concrete/BrandManager.cs
@@ -44,6 +44,8 @@ namespace Business.Concrete
 
         public IDataResult<List<Brand>> GetAll()
         {
+
+
             return new SuccessDataResult<List<Brand>>(_brandDal.GetAll(),Messages.Listed);
         }
 
