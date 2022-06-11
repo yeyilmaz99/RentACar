dec04cb491893c37640412c7fd039f96568f9372
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-06-11 20:55:41 +0300

    18.gün ödev

--- DataAccess/Abstract/ICarDal.cs
+++ DataAccess/Abstract/ICarDal.cs
@@ -4,6 +4,7 @@ using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,7 @@ namespace DataAccess.Abstract
 {
     public interface ICarDal : IEntityRepository<Car>
     {
-        List<CarDetailDto> GetCarDetails();
+        List<CarDetailDto> GetCarsDetails(Expression<Func<Car, bool>> filter = null);
+        CarDetailDto GetCarDetail(Expression<Func<Car, bool>> filter);
     }
 }
