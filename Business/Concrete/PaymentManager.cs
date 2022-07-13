86882231b3d2b77fe4f92631eb9720df7bd873fd
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-07-13 13:43:00 +0300

    19-gün payment

---
+++ Business/Concrete/PaymentManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class PaymentManager : IPaymentService
+    {
+        IPaymentDal _paymentDal;
+
+        public PaymentManager(IPaymentDal paymentDal)
+        {
+            _paymentDal = paymentDal;
+        }
+
+        public IResult Add(Payment payment)
+        {
+            _paymentDal.Add(payment);
+            return new SuccessResult(Messages.Added);
+        }
+    }
+}
