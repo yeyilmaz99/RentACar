59af84012f33927a0ad3e16e664e8f2101ea640e
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-07-13 13:50:05 +0300

    19-gün payment

---
+++ WebAPI/Controllers/PaymentController.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class PaymentController : Controller
+    {
+        IPaymentService _paymentService;
+
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+
+        [HttpPost("add")]
+        public IActionResult Add(Payment payment)
+        {
+            var result = _paymentService.Add(payment);
+            if (result.Success == true)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
