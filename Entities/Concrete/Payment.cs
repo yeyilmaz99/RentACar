516f7eb024fab35ea33dfcfe5f32d005688197a5
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-07-13 13:43:07 +0300

    19-gün payment

---
+++ Entities/Concrete/Payment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class Payment : IEntity
+    {
+        public int Id { get; set; }
+        public string CardName { get; set; } 
+        public string CardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public string SecurityCode { get; set; }    
+    }
+}
