a68baae4914ac206d8a7c6e48f884817327cd53b
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-19 22:02:52 +0300

    userFavorites

---
+++ Business/Concrete/FavoriteManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class FavoriteManager : IFavoriteService
+    {
+        IFavoriteDal _favoriteDal;
+
+        public FavoriteManager(IFavoriteDal favoriteDal)
+        {
+            _favoriteDal = favoriteDal;
+        }
+
+        public IDataResult<List<Favorite>> GetAllFavorites(int userId)
+        {
+            return new SuccessDataResult<List<Favorite>>(_favoriteDal.GetAll(user => user.UserId == userId));
+        }
+
+        public IResult Add(Favorite favorite)
+        {
+            _favoriteDal.Add(favorite);
+            return new SuccessResult(Messages.Added);
+        }
+
+        public IResult Delete(Favorite favorite)
+        {
+            _favoriteDal.Delete(favorite);
+            return new SuccessResult(Messages.Deleted);
+        }
+    }
+}
