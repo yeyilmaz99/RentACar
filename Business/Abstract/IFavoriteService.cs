27929a3bcbf3d48146ff447b6f2f35791d5d9835
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-19 22:02:49 +0300

    userFavorites

---
+++ Business/Abstract/IFavoriteService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Results.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IFavoriteService
+    {
+        IDataResult<List<Favorite>> GetAllFavorites(int userId);
+        IResult Add(Favorite favorite);
+        IResult Delete(Favorite favorite);
+    }
+}
