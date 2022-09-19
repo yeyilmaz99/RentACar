9d0242b24fcdb02a6e0a351367e3208848e34e2b
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-19 22:28:28 +0300

    userFavorites

--- Entities/DTOs/userFavoriteDto.cs
+++ Entities/DTOs/userFavoriteDto.cs
@@ -8,6 +8,13 @@ namespace Entities.DTOs
 {
     public class UserFavoriteDto
     {
-
+        public int UserId { get; set; }
+        public int CarId { get; set; }
+        public string UserName { get; set; }    
+        public string CarName { get; set; }
+        public string BrandName { get; set; }
+        public string ColorName { get; set; }
+        public double DailyPrice { get; set; }
+        public string Description { get; set; }
     }
 }
