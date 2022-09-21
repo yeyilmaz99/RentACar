775256f72976d6216cfe88d3ba14d9fd09c7fb65
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-21 19:32:03 +0300

    userFavorites

--- DataAccess/Concrete/EntityFramework/EfFavoriteDal.cs
+++ DataAccess/Concrete/EntityFramework/EfFavoriteDal.cs
@@ -13,12 +13,13 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfFavoriteDal : EfEntityRepositoryBase<Favorite, MyDatabaseContext>, IFavoriteDal
     {
-        public List<UserFavoriteDto> GetFavorites(Expression<Func<Car, bool>> filter = null)
+
+        public List<UserFavoriteDto> GetUsersFavorites(Expression<Func<Favorite, bool>> filter)
         {
             using (MyDatabaseContext context = new MyDatabaseContext())
             {
-                var result = from c in context.Cars.Where(filter)
-                    join fav in context.Favorite on c.Id equals fav.CarId
+                var result = from fav in context.Favorites.Where(filter)
+                    join c in context.Cars on fav.CarId equals c.Id
                     join b in context.Brands on c.BrandId equals b.BrandId
                     join clr in context.Colors on c.ColorId equals clr.ColorId
                     join user in context.Users on fav.UserId equals user.Id
@@ -32,6 +33,29 @@ namespace DataAccess.Concrete.EntityFramework
                         ColorName = clr.ColorName,
                         Description = c.Description,
                         UserName = user.FirstName + " " + user.LastName,
+                    };
+                return result.ToList();
+            }
+        }
+        public List<UserFavoriteDto> GetFavoritesDetails(Expression<Func<Favorite, bool>> filter = null)
+        {
+            using (MyDatabaseContext context = new MyDatabaseContext())
+            {
+                var result = from fav in filter == null ? context.Favorites : context.Favorites.Where(filter)
+                    join c in context.Cars on fav.CarId equals c.Id
+                    join usr in context.Users on fav.UserId equals usr.Id
+                    join b in context.Brands on c.BrandId equals b.BrandId
+                    join clr in context.Colors on c.ColorId equals clr.ColorId
+                    select new UserFavoriteDto
+                    {
+                        CarId = c.Id,
+                        UserId = fav.UserId,
+                        BrandName = b.BrandName,
+                        CarName = c.CarName,
+                        DailyPrice = c.DailyPrice,
+                        ColorName = clr.ColorName,
+                        Description = c.Description,
+                        UserName = usr.FirstName + " " + usr.LastName
                     };
                 return result.ToList();
             }
