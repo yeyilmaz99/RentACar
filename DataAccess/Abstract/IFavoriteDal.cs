d27a73ac28cc807d9089b68b5ca37265e1efd363
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-21 19:32:01 +0300

    userFavorites

--- DataAccess/Abstract/IFavoriteDal.cs
+++ DataAccess/Abstract/IFavoriteDal.cs
@@ -5,11 +5,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities.DTOs;
+using System.Linq.Expressions;
 
 namespace DataAccess.Abstract
 {
     public interface IFavoriteDal : IEntityRepository<Favorite>
     {
+        List<UserFavoriteDto> GetUsersFavorites(Expression<Func<Favorite, bool>> filter);
+        List<UserFavoriteDto> GetFavoritesDetails(Expression<Func<Favorite, bool>> filter = null);
 
     }
 }
