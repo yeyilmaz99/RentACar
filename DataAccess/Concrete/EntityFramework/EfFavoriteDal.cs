33b96c1a9eed32201ee373552b151580182e00b7
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-21 19:47:27 +0300

    userFavorites

--- DataAccess/Concrete/EntityFramework/EfFavoriteDal.cs
+++ DataAccess/Concrete/EntityFramework/EfFavoriteDal.cs
@@ -13,30 +13,6 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfFavoriteDal : EfEntityRepositoryBase<Favorite, MyDatabaseContext>, IFavoriteDal
     {
-
-        public List<UserFavoriteDto> GetUsersFavorites(Expression<Func<Favorite, bool>> filter)
-        {
-            using (MyDatabaseContext context = new MyDatabaseContext())
-            {
-                var result = from fav in context.Favorites.Where(filter)
-                    join c in context.Cars on fav.CarId equals c.Id
-                    join b in context.Brands on c.BrandId equals b.BrandId
-                    join clr in context.Colors on c.ColorId equals clr.ColorId
-                    join user in context.Users on fav.UserId equals user.Id
-                    select new UserFavoriteDto
-                    {
-                        CarId = c.Id,
-                        UserId = fav.UserId,
-                        BrandName = b.BrandName,
-                        CarName = c.CarName,
-                        DailyPrice = c.DailyPrice,
-                        ColorName = clr.ColorName,
-                        Description = c.Description,
-                        UserName = user.FirstName + " " + user.LastName,
-                    };
-                return result.ToList();
-            }
-        }
         public List<UserFavoriteDto> GetFavoritesDetails(Expression<Func<Favorite, bool>> filter = null)
         {
             using (MyDatabaseContext context = new MyDatabaseContext())
