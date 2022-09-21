5a306ee3ca3cd33e9c4d779c4b023bd2b3f6e8e1
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-21 19:32:07 +0300

    userFavorites

---
+++ WebAPI/Controllers/FavoritesController.cs
@@ -0,0 +1,54 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FavoritesController : ControllerBase
+    {
+         IFavoriteService _favoriteService;
+
+         public FavoritesController(IFavoriteService favoriteService)
+         {
+             _favoriteService = favoriteService;
+         }
+
+
+         [HttpGet("getUsersFavorites")]
+         public IActionResult getUsersFavorites(int userId)
+         {
+             var result = _favoriteService.GetUsersFavorites(userId);
+             if (result.Success)
+             {
+                 return Ok(result);
+             }
+
+             return BadRequest(result);
+         }
+         [HttpGet("getall")]
+         public IActionResult GetAll(int userId)
+         {
+             var result = _favoriteService.GetAllFavorites(userId);
+             if (result.Success)
+             {
+                 return Ok(result);
+             }
+
+             return BadRequest(result);
+         }
+
+         [HttpGet("getfavoritesdetails")]
+         public IActionResult getDetails()
+         {
+             var result = _favoriteService.GetFavoritesDetails();
+             if (result.Success)
+             {
+                 return Ok(result);
+             }
+
+             return BadRequest(result);
+         }
+    }
+}
