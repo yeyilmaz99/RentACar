99f943a22edde945d0eff0e202c239d2ec790806
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-22 23:49:47 +0300

    userFavorites

--- Business/Abstract/IFavoriteService.cs
+++ Business/Abstract/IFavoriteService.cs
@@ -14,7 +14,7 @@ namespace Business.Abstract
         IDataResult<List<Favorite>> GetAllFavorites(int userId);
         IResult Add(Favorite favorite);
         IResult Delete(Favorite favorite);
-        IDataResult<List<UserFavoriteDto>> GetFavoritesDetails();
         IDataResult<List<UserFavoriteDto>> GetFavoritesByUserId(int userId);
+        IResult CheckIfAlreadyAddedToFavorites(int carId,int userId);
     }
 }
