2c03ec335e297c345855861c22466598f48bcf38
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-26 19:32:14 +0300

    rentalBugFix

--- DataAccess/Abstract/IRentalDal.cs
+++ DataAccess/Abstract/IRentalDal.cs
@@ -6,11 +6,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Entities.DTOs;
+using System.Linq.Expressions;
 
 namespace DataAccess.Abstract
 {
     public interface IRentalDal : IEntityRepository<Rental>
     {
-        List<RentalDetailDto> GetRentalDetails();
+        List<RentalDetailDto> GetRentalDetails(Expression<Func<Rental, bool>> filter = null);
     }
 }
