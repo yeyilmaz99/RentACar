e6af03fbdb9afb9cc066c1bc3d84cdd604716036
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-26 19:32:26 +0300

    rentalBugFix

--- WebAPI/Controllers/RentalsController.cs
+++ WebAPI/Controllers/RentalsController.cs
@@ -38,6 +38,17 @@ namespace WebAPI.Controllers
             return BadRequest(result);
         }
 
+        [HttpGet("getrentaldetailsbyuserid")]
+        public IActionResult GetRentalDetailsByUserId(int userId)
+        {
+            var result = _rentalService.GetRentalDetailsByUserId(userId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
         [HttpGet("getbyid")]
         public IActionResult Get(int id)
         {
