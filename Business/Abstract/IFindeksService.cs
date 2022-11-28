669c980f5311003dc5536681b27bed013d32f6dd
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-11-28 03:35:48 +0300

    findeks

---
+++ Business/Abstract/IFindeksService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Core.Utilities.Results.Abstract;
+using Entities.Concrete;
+using System.Collections.Generic;
+
+namespace Business.Abstract
+{
+	public interface IFindeksService
+	{
+        IDataResult<List<Findeks>> GetAll();
+        IDataResult<Findeks> GetById(int userId);
+        IResult Add(Findeks findeks);
+        IResult Update(Findeks findeks);
+        IResult Delete(Findeks findeks);
+    }
+}
+
