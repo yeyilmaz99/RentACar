92da90e2118da7e463f0663a51d9a541c8fab4f3
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-11-28 03:55:11 +0300

    findeks

---
+++ WebAPI/Controllers/FindeksController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class FindeksController : Controller
+    {
+        IFindeksService _findeksService;
+
+        public FindeksController(IFindeksService findeksService)
+        {
+            _findeksService = findeksService;
+        }
+
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _findeksService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+
+        [HttpGet("getByUserId")]
+        public IActionResult GetFavoritesDetailsByUserId(int userId)
+        {
+            var result = _findeksService.GetById(userId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+
+
+
+    }
+}
+
