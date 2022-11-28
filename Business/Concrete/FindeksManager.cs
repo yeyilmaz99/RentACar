d14100fe784994cf3fc5ddb8730b439fe9687012
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-11-28 03:38:37 +0300

    findeks

--- Business/Concrete/FindeksManager.cs
+++ Business/Concrete/FindeksManager.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using Business.Abstract;
+using Business.Constants;
 using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
 using DataAccess.Abstract;
 using Entities.Concrete;
 
@@ -18,7 +20,8 @@ namespace Business.Concrete
 
         public IResult Add(Findeks findeks)
         {
-            throw new NotImplementedException();
+            _findeksDal.Add(findeks);
+            return new SuccessResult(Messages.Added);
         }
 
         public IResult Delete(Findeks findeks)
