6382ce143d0902a2d7f2dc345b36aa74277d798b
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-12-03 23:57:05 +0300

    findeks

--- Business/Abstract/IFindeksService.cs
+++ Business/Abstract/IFindeksService.cs
@@ -10,7 +10,7 @@ namespace Business.Abstract
         IDataResult<List<Findeks>> GetAll();
         IDataResult<Findeks> GetById(int userId);
         IResult Add(Findeks findeks);
-        IResult CheckIfFPSufficient(int carFP, int userId);
+        IResult CheckIfFPSufficient(int carFP, IDataResult<Findeks> findeks);
         IResult Update(Findeks findeks);
         IResult Delete(Findeks findeks);
     }
