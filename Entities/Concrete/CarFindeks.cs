2695f5f791ce1cb82c3a76c13a0c3a7ba3d6133f
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-12-04 02:13:04 +0300

    CarFindeks

---
+++ Entities/Concrete/CarFindeks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Core.Entities;
+
+namespace Entities.Concrete
+{
+	public class CarFindeks : IEntity
+	{
+		public int Id { get; set; }
+		public int CarId { get; set; }
+		public int FindeksPoint { get; set; }
+
+	}
+}
+
