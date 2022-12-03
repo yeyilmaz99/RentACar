118ea4264c8de9915d927f86a0612d507d2d7915
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-12-04 02:12:57 +0300

    CarFindeks

---
+++ DataAccess/Concrete/EntityFramework/EfCarFindeksDal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+	public class EfCarFindeksDal: EfEntityRepositoryBase<CarFindeks, MyDatabaseContext>, ICarFindeksDal
+	{
+	}
+}
+
