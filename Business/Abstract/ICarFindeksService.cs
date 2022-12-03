fd117c331a84c418857ae6f2f06eb682aae98dbd
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-12-04 02:12:39 +0300

    CarFindeks

---
+++ Business/Abstract/ICarFindeksService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Core.Utilities.Results.Abstract;
+using System.Collections.Generic;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+	public interface ICarFindeksService
+	{
+        IDataResult<List<CarFindeks>> GetAll();
+        IDataResult<CarFindeks> GetById(int id);
+        IResult Add(CarFindeks carFindeks);
+        IResult Update(CarFindeks carFindeks);
+        IResult Delete(CarFindeks carFindeks);
+    }
+}
+
