c5aa7792288c67b0c07709bd81438869f49c1f71
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-12-28 05:05:56 +0300

    userDal update

--- DataAccess/Abstract/IUserDal.cs
+++ DataAccess/Abstract/IUserDal.cs
@@ -5,11 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using Core.DataAccess;
 using Core.Entities.Concrete;
+using Entities.DTOs;
 
 namespace DataAccess.Abstract
 {
     public interface IUserDal : IEntityRepository<User>
     {
         List<OperationClaim> GetClaims(User user);
+        List<UserDetailDto> GetUserDetails();
     }
 }
