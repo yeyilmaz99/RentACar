233c8a397d6cf99e0664503f1a04a27e48f5df7c
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-12-28 05:06:29 +0300

    userDetailDto

---
+++ Entities/DTOs/UserDetailDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Entities.DTOs
+{
+	public class UserDetailDto
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public bool Status { get; set; }
+		public int FindeksPoint { get; set; }
+
+	}
+}
+
